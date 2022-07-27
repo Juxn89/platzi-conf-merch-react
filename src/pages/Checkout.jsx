83fd2b8d8891419f1b054dabb0a9790d71bd66de
Juan Gómez <gomezj01@stepstone.com>
2022-07-27 13:18:47 -0600

    Added layout and style to Checkout page.

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,6 +3,22 @@ import '../styles/components/Checkout.css';
 
 export const Checkout = () => {
   return (
-    <h1>Checkout</h1>
+    <div className='Checkout'>
+      <div className="Checkout-content">
+        <h3>Lista de Pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item Name</h4>
+            <span>Item price</span>            
+          </div>
+          <button type='button'>Eliminar</button>
+        </div>
+      </div>
+
+      <div className='Checkout-sidebar'>
+        <h3>Precio total:</h3>
+        <button type='button'>Continuar pedido</button>
+      </div>
+    </div>
   )
 }
